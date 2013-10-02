acacb0f4cbe741742d0a2feca2c5053343d26a3b
paulo.ortins@gmail.com <paulo.ortins@gmail.com>
2013-10-01 22:13:30 -0300

    Added file extensions chart

--- output/httpd/js/graphs.js
+++ output/httpd/js/graphs.js
@@ -9,10 +9,9 @@ Graphs.prototype.setDraggable = function() {
 
 Graphs.prototype.createReferences = function() {
 	this.weekdayGraph = this.createWeekdayGraph();
-
     this.timeoftheday = this.createTimeOfTheDayGraph();
-
     this.time = this.createTimeGraph();
+    this.fileTypeGraph = this.createFileTypeGraph();
 };
 
 Graphs.prototype.update = function (commitData, emailData) {
@@ -26,6 +25,12 @@ Graphs.prototype.update = function (commitData, emailData) {
 
 	self.time.highcharts().series[0].setData(self.generateDataPerTime(commitData));
 	self.time.highcharts().series[1].setData(self.generateDataPerTime(emailData));
+
+    var extensionData = self.generateDataPerFileExtension(commitData);
+    self.fileTypeGraph.highcharts().xAxis[0].setCategories(extensionData.extensionList);
+    self.fileTypeGraph.highcharts().series[0].setData(extensionData.countList);
+    self.fileTypeGraph.highcharts().setSize(self.fileTypeGraph.highcharts().width, extensionData.countList.length * 15);
+    $("#fileType").height(extensionData.countList.length * 15);
 };
 
 Graphs.prototype.generateDataPerWeekday = function(data) {
@@ -45,8 +50,8 @@ Graphs.prototype.generateDataPerTimeOfTheDay = function(data) {
 	var hour;
 	
 	for (var i = 0; i < data.length; i++) {
-		unit = data[i];
-		hour = unit.time[0] + "" + unit.time[1];
+		time = data[i].time.trim();
+		hour = time[0] + "" + time[1];
 		var position = parseInt(hour/2);
 		timeOfTheDayData[position] = timeOfTheDayData[position] + 1
 	};
@@ -91,6 +96,60 @@ Graphs.prototype.generateDataPerTime = function(data) {
 	};
 
 	return times;
+};
+
+Graphs.prototype.generateDataPerFileExtension = function(data) {
+    var extensions = {};
+
+    for (var i = 0; i < data.length; i++) {
+        var commit = data[i];
+        for (var j = 0; j < commit.paths.length; j++) {
+            var fileExtension = this.getFileExtension(commit.paths[j]);
+
+            if (extensions[fileExtension] == undefined) {
+                extensions[fileExtension] = 0;
+            } 
+
+            extensions[fileExtension] = extensions[fileExtension] + 1;
+        };
+    }
+
+    var extensionData = [];    
+
+    for (var extension in extensions) {
+        extensionData.push({extension: extension, count: extensions[extension]});
+    }
+
+    extensionData = extensionData.sort(function(a,b) {return a.count - b.count}).reverse();
+
+    var extensionObject = {};
+    extensionObject.extensionList = [];
+    extensionObject.countList = [];
+
+    for (var i = 0; i < extensionData.length; i++) {
+        var extension = extensionData[i];
+        extensionObject.extensionList.push(extension.extension);
+        extensionObject.countList.push(extension.count);
+    };   
+
+    return extensionObject;
+};
+
+Graphs.prototype.getFileExtension = function(path) {
+    var pieces = path.split('/');
+    var fileName = pieces[pieces.length - 1];
+
+    var filePieces = fileName.split('.');
+
+    if (filePieces.length == 1) {
+        return 'NotSpecified'
+    } else {
+        var extension = '';
+        for (var i = 1; i < filePieces.length; i++) {
+            extension += '.' + filePieces[i];
+        };
+        return extension;
+    }
 };
 
 Graphs.prototype.createWeekdayGraph = function() {
@@ -221,6 +280,52 @@ Graphs.prototype.createTimeGraph = function() {
                 data: []
             }, {
                 name: 'Email',
+                data: []
+            }]
+        });
+};
+
+Graphs.prototype.createFileTypeGraph = function() {
+    return $('#file-type-graph').highcharts({
+            chart: {
+                type: 'bar',
+                width: $("#fileType").width() - 30,
+                height: $("#fileType").height()
+            },
+            exporting: {
+                enabled: false
+            },
+            title: {
+                text: 'Commits by File Extension',
+                x: -70 //center
+            },
+            xAxis: {
+                categories: [],
+                tickPixelInterval: 200
+            },
+            yAxis: {
+                title: {
+                    text: ''
+                },
+                min: 0,
+                plotLines: [{
+                    value: 0,
+                    width: 1,
+                    color: '#808080'
+                }]
+            },
+            tooltip: {
+                valueSuffix: ''
+            },
+            legend: {
+                layout: 'horizontal',
+                align: 'right',
+                verticalAlign: 'top',
+                borderWidth: 0,
+                floating: true
+            },
+            series: [{
+                name: 'File Extensions',
                 data: []
             }]
         });
